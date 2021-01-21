1b05c795fe079b5964dbdb4b04a7f3e40818435d
Mehraj_Ahmed <mehraj24158@gmail.com>
2021-01-20 20:30:47 -0500

    removed build files

--- build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Mehraj_Ahmed/Workspace/SSE_554/P1/googletest
-# Build directory: C:/Users/Mehraj_Ahmed/Workspace/SSE_554/P1/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
